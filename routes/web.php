e9b669883d69e69bdd943e7c85b9c7b39187fa93
Yevhenii Hrzhenda <jonleev@gmail.com>
2023-06-29 20:31:15 +0300

    first route, component & setup in app.js

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
+use App\Http\Controllers\ApplicationController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/admin/dashboard', function () {
-    return view('dashboard');
-});
+//Route::get('/admin/dashboard', function () {
+//    return view('dashboard');
+//});
+
+Route::get('{view}', ApplicationController::class)->where('view', '(.*)');
