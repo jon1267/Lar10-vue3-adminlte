dd9b2af96fda67caddf70da7f9fb3ebb751d0ec7
Yevhenii Hrzhenda <jonleev@gmail.com>
2023-07-05 22:32:23 +0300

    web route && it axios part, get users

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ApplicationController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\UserController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,5 +11,7 @@ Route::get('/', function () {
 //Route::get('/admin/dashboard', function () {
 //    return view('dashboard');
 //});
+
+Route::get('/api/users', [UserController::class, 'index']);
 
 Route::get('{view}', ApplicationController::class)->where('view', '(.*)');
