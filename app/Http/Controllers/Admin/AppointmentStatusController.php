a7204d2b8a25e82b21a64d6f86671d813f4593b7
Yevhenii Hrzhenda <jonleev@gmail.com>
2023-09-06 23:13:04 +0300

    route && controller for appoints statuses

---
+++ app/Http/Controllers/Admin/AppointmentStatusController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AppointmentStatusController extends Controller
+{
+    public function getStatusWithCount()
+    {
+        return [];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ApplicationController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Admin\AppointmentController;
+use App\Http\Controllers\Admin\AppointmentStatusController;
 
 Route::get('/admin/dashboard', function () {
     return view('dashboard');
@@ -23,6 +24,6 @@ Route::put('/api/users/{user}', [UserController::class, 'update']);
 Route::delete('/api/users/{user}', [UserController::class, 'destroy']);
 Route::delete('/api/users', [UserController::class, 'bulkDelete']);
 
-Route::get('/api/appointment-status',[]);
+Route::get('/api/appointment-status',[AppointmentStatusController::class, 'getStatusWithCount']);
 Route::get('/api/appointments', [AppointmentController::class, 'index']);
 Route::get('{view}', ApplicationController::class)->where('view', '(.*)');
