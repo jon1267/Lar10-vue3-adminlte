ed75097a66949dbb80815ec0a6717c09233069f6
Yevhenii Hrzhenda <jonleev@gmail.com>
2023-09-26 10:06:27 +0300

    vee-validate appointment form

--- app/Http/Controllers/Admin/AppointmentController.php
+++ app/Http/Controllers/Admin/AppointmentController.php
@@ -33,6 +33,11 @@ class AppointmentController extends Controller
 
     public function store()
     {
+        request()->validate([
+            'title' => 'required',
+            'description' => 'required',
+        ]);
+
         Appointment::create([
             'title' => request('title'),
             'client_id' => 2,
